c18a81b5ee8e0735f801eb1191a7cc67d892296a
KJH <daeding7@naver.com>
2022-07-05 12:47:04 +0900

    뷰페이저 뼈대 완성

--- app/src/main/java/com/kim/viewpagerpractice3/MainActivity.kt
+++ app/src/main/java/com/kim/viewpagerpractice3/MainActivity.kt
@@ -2,10 +2,23 @@ package com.kim.viewpagerpractice3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.transition.FragmentTransitionSupport
+import com.kim.viewpagerpractice3.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mMainViewPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mMainViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        firstViewPager.adapter = mMainViewPagerAdapter
+
+        firstViewPager.offscreenPageLimit = 5
+
     }
 }
---
+++ app/src/main/java/com/kim/viewpagerpractice3/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.kim.viewpagerpractice3.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.kim.viewpagerpractice3.fragments.AddressFragment
+import com.kim.viewpagerpractice3.fragments.AgeFragment
+import com.kim.viewpagerpractice3.fragments.NameFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+        if(position == 0){
+
+            return NameFragment()
+
+            }
+
+        else if(position == 1){
+
+            return AgeFragment()
+        }
+
+        else{
+
+            return AddressFragment()
+        }
+    }
+}
